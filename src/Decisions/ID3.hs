bc93de59740465783b93246831cb7847abfd9636
Richard Protasov <richardprotasov@gmail.com>
2016-10-23 15:45:32 -0400

    Move out all helper functions from id3 module

--- src/Decisions/ID3.hs
+++ src/Decisions/ID3.hs
@@ -1,20 +1,9 @@
 module Decisions.ID3
 ( prune
 , id3
-, commonClassOf
-, ispure
-, largestGain
-, largest
-, gain
-, entropy
-, entropy'
-, add
-, (%)
 ) where
 
 import Decisions
-import Control.Applicative
-import qualified Data.Map as Map
 import Data.List (delete)
 
 prune
@@ -35,82 +24,3 @@ id3 (set, fs, tt@(att, _))
           leaf = Leaf (att, commonClassOf set tt)
           make c sub = Decision c $
             if isempty sub then leaf else id3 (sub,fs',tt)
-
-commonClassOf
-  :: Set
-  -> Field
-  -> Class
-commonClassOf set (attr, classes) = c
-  where (_, c) = foldl largest ratio ratios
-        (ratio:ratios) = [ ((%) set (length set) attr c, c) | c <- classes ]
-
-ispure
-  :: Set
-  -> Field
-  -> Bool
-ispure = (.:) (0 ==) entropy
-
-largestGain
-  :: Set
-  -> [Field]
-  -> Field
-  -> Field
-largestGain set fields for = field
-  where (_, field) = foldl largest canidate canidates
-        (canidate:canidates) = [ (gain set f for, f) | f <- fields ]
-
-largest
-  :: (Ord a, Floating a)
-  => (a, b)
-  -> (a, b)
-  -> (a, b)
-largest current@(currentAmt,_) canidate@(canidateAmt,_)
-  | canidateAmt > currentAmt = canidate
-  | otherwise = current
-
-gain
-  :: (Ord a, Floating a)
-  => Set
-  -> Field
-  -> Field
-  -> a
-gain set field@(fA, fCs) target@(tA, tCs) = e - e'
-  where e  = entropy set target
-        e' = add [ calculate s | s <- orderByClass set field ]
-        calculate (c, sub)
-          = (%) sub (length set) fA c * entropy sub target
-
-entropy
-  :: (Ord a, Floating a)
-  => Set
-  -> Field
-  -> a
-entropy set (attr, classes) = add [ entropy' set attr c | c <- classes ]
-
-entropy'
-  :: (Ord a, Floating a)
-  => Set
-  -> Attribute
-  -> Class
-  -> a
-entropy' set attribute c
-  | ratio /= 0 = abs $ ratio * logBase 2 ratio
-  | otherwise  = 0
-      where ratio = (%) set (length set) attribute c
-
-add
-  :: Num a
-  => [a]
-  -> a
-add = foldl (+) 0
-
-(%)
-  :: (Ord a, Floating a)
-  => Set
-  -> Int
-  -> Attribute
-  -> Class
-  -> a
-(%) set size attr c = fromIntegral in' / fromIntegral of'
-  where in' = length $ filter (\p -> get p attr == c) set
-        of' = toInteger size
